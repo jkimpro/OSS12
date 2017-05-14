f116cb945ab308ccdfdc1ddaf3d9a10ce886e87a
okjcd123 <okjcd123@naver.com>
2017-05-14 13:04:23 +0900

    newfile

--- SolarSystem12/freeglutProject3D/main1.cpp
+++ SolarSystem12/freeglutProject3D/main1.cpp
@@ -1,7 +1,14 @@
-//---------------------------------------------------------------------------
-// Dan Cristia, Rotaru
-//---------------------------------------------------------------------------
-//OpenSoure Software Digital Contents 김준혁 문희호 이상협 정지혜
+/*
+---------------------------------------------------------------------------
+ Copy from Dan Cristia, Rotaru
+ https://github.com/RotaruDan/SolarSystem
+---------------------------------------------------------------------------
+ OpenSource Software Project (https://github.com/okjcd123/OSS12)
+ Digital Contents 김준혁 문희호 이상협 정지혜
+
+ Date of preparation (작성일):					2017년 5월 12일
+ Date of final modification (최종 수정일) :		2017년 5월 14일
+*/
 #include <Windows.h>
 #include <gl/GL.h>
 #include <gl/GLU.h>
