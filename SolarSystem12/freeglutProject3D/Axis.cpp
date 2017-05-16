678e387491fc961c3bc8a24f4bf9c00966b83eb5
kkukkukkakka <ilovesz@naver.com>
2017-05-16 12:42:48 +0900

    Axis.cpp(M)

--- SolarSystem12/freeglutProject3D/Axis.cpp
+++ SolarSystem12/freeglutProject3D/Axis.cpp
@@ -1,25 +1,43 @@
-//---------------------------------------------------------------------------
-// Dan Cristian, Rotaru
-//---------------------------------------------------------------------------
-#include "Axis.h"
+/*
+---------------------------------------------------------------------------
+ Copied from Dan Cristia, Rotaru
+ https://github.com/RotaruDan/SolarSystem
+---------------------------------------------------------------------------
+ OpenSource Software Project (https://github.com/okjcd123/OSS12)
+ Digital Contents 김준혁 문희호 이상협 정지혜
+ Date of preparation (작성일):					2017년 5월 12일
+ Date of final modification (최종 수정일):			2017년 5월 16일
+*/
 
-	Axis::Axis(){
+
+---------------------------------------------------------------------------
+
+#include "Axis.h"	//헤더파일 선언
+
+
+	//X Y Z축 그리기
+	Axis::Axis()	//Axis  객체 생성자
+	{
 
 	}
 
-	Axis::~Axis(){
+	Axis::~Axis()	//Axis 객체 소멸자
+	{
 
 	}
 
-	void Axis::draw(){
+	void Axis::draw()	//X Y Z축(선) 그리기
+	{
 		glBegin( GL_LINES );
 		glVertex3f(0, 0, 0);      // origin
-		glVertex3f(200, 0, 0);	  // x   
+		glVertex3f(200, 0, 0);    // X좌표
  
 		glVertex3f(0, 0, 0);      // center
-		glVertex3f(0, 200, 0);	  // y
+		glVertex3f(0, 200, 0);	  // Y좌표
 
 		glVertex3f(0, 0, 0);      // origin
-		glVertex3f(0, 0, 200);	  // z   
+		glVertex3f(0, 0, 200);	  // Z좌표   
 		glEnd();
 	}
+
+
