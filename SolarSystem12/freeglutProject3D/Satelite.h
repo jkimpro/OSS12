4060177e874767a4550dcb2c8ccca97f46b195cc
MoonHeeHo <heeho9307@naver.com>
2017-05-21 19:16:17 +0900

    [mod] Satelite.h 주석 추가 - 파일명, 목적, 사용 방식, 제한 사항 나타내는 주석 추가 - private 멤버 변수 scale 설명 추가 - draw(), drawTriangle(), drawPiramid(), drawSquare() 설명 추가

--- SolarSystem12/freeglutProject3D/Satelite.h
+++ SolarSystem12/freeglutProject3D/Satelite.h
@@ -1,24 +1,31 @@
-//---------------------------------------------------------------------------
-// Dan Cristian, Rotaru
-//---------------------------------------------------------------------------
+/*******************************************************************************************
+파 일 명 : Satellite.h
+목    적 : Satellite 클래스의 선언부
+사용방식 : Header Files 내부 Objects에 위치
+제한사항 :	Satellite 클래스가 Actor 클래스를 상속받기 때문에 클래스 내에서 draw 함수 선언 필요
+********************************************************************************************/
+
 #ifndef SateliteH
 #define SateliteH
 
 #include "Actor.h"
 
-class Satelite : public Actor {
-
+/* Actor 클래스를 상속받는 Satellite 클래스 선언 */
+class Satelite : public Actor 
+{
+	/* Satellite 클래스의 private 멤버 */
 	private:
-		GLdouble scale;
+		GLdouble scale;                       //크기 : 인공위성의 높이의 반을 나타냄
 
+	/* Satellite 클래스의 public 멤버 */
 	public:
-		Satelite();
-		Satelite(GLdouble scale);
-		~Satelite();
-		void draw();
-		void drawTriangle();
-		void drawPiramid();
-		void drawSquare();
+		Satelite();                           //Satellite 객체 생성자
+		Satelite(GLdouble scale);             //매개 변수 scale의 값을 인자로 하는 Satellite 생성자
+		~Satelite();                          //Satellite 객체 소멸자
+		void draw();                          //객체를 화면에 그려주는 함수
+		void drawTriangle();                  //삼각형 도형 그려주는 함수
+		void drawPiramid();                   //사각뿔 도형 그려주는 함수
+		void drawSquare();                    //사각형 도형 그려주는 함수
 };
 
 #endif
