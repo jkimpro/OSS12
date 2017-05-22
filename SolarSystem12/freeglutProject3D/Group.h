722e8972edf55920651c6dd610f2202e0451cace
MoonHeeHo <heeho9307@naver.com>
2017-05-22 13:01:16 +0900

    [mod] Group.h 주석 추가 - 파일명, 목적, 사용방식, 제한사항 설명 추가 - private 멤버 변수 children, 멤버 함수 drawChildren() 함수 설명 주석 추가 - Group 클래스 객체 생성자, 소멸자 설명 주석 추가 - public 멤버 함수 addchildren() 설명 주석 추가 - public 멤버 함수 draw() 설명 주석 추가

--- SolarSystem12/freeglutProject3D/Group.h
+++ SolarSystem12/freeglutProject3D/Group.h
@@ -1,27 +1,34 @@
-//---------------------------------------------------------------------------
-// Dan Cristian, Rotaru
-//---------------------------------------------------------------------------
+/********************************************************************************************
+	파 일 명 : Group.h
+	목    적 : Group 클래스의 선언부
+	사용방식 : Header Files 내부에 위치
+	제한사항 : Group 클래스가 Actor 클래스를 상속받기 때문에 클래스 내에서 draw 함수 선언 필요
+*********************************************************************************************/
+
 #include "Actor.h"
 #include "vector"
 
 #ifndef GroupH
 #define GroupH
 
-using namespace std;
-
-class Group : public Actor {
-
+/* Actor 클래스를 상속 받는 Group 클래스 */
+class Group : public Actor                                
+{                               
+	/* Group 클래스의 private 멤버 */
 	private:
-		vector<Actor*> children;
+		vector<Actor*> children;                           //children : Actor 객체들을 자유롭게 추가하거나 삭제할 수 있는 벡터
 
-		void drawChildren();
+		/* vector는 표준 라이브러리에서 지원하는 std::vector의 멤버 변수
+		   크기 변경 불가능한 배열 대신 사용 */
+		void drawChildren();                               //children 벡터 안에 있는 모든 Actor 객체들을 화면에 그리는 함수 
 
+	/* Group 클래스의 public 멤버 */
 	public:
-		Group();
-		~Group();
-
-		void addChildren(Actor* actor);
-		void draw();
+		Group();                                           //Group 객체 생성자
+		~Group();                                          //Group 객체 소멸자
+	
+		void addChildren(Actor* actor);                    //children 벡터에 Actor 객체를 추가하는 함수
+		void draw();	                                   //도형 객체를 화면에 그려주는 함수
 
 };
 
